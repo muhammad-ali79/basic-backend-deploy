2c48c5e67f12266b8f2406c5ec4c67c0dad8e6ed
muhammad-ali79 <alihelium369@gmail.com>
2024-02-01 16:19:40 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+// const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/halwa", (req, res) => {
+  res.send("this is the page for buying halwa");
+});
+
+app.get("/node", (req, res) => {
+  res.send(`<h1>This is the page to learn node</h1>
+
+  <a herf="https://nodejs.org/>
+  `);
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`);
+});
